8aea1659838f935d353f176543c1a030f33769aa
Nada Amin <namin@alum.mit.edu>
2018-04-12 16:58:45 +0100

    factorial works cc @webyrd

--- clpsmt-tests.scm
+++ clpsmt-tests.scm
@@ -26,3 +26,29 @@
          (z/assert `(= ,x 0))
          (== x q)))))
   '(0))
+
+(define faco
+  (lambda (n out)
+    (conde ((z/assert `(= ,n 0))
+            (z/assert `(= ,out 1)))
+           ((z/assert `(> ,n 0))
+            (z/fresh
+             (lambda (n-1)
+               (fresh ()
+                 (z/assert `(= (- ,n 1) ,n-1))
+                 (z/fresh
+                  (lambda (r)
+                    (fresh ()
+                      (z/assert `(= (* ,n ,r) ,out))
+                      (faco n-1 r)))))))))))
+
+(test "4"
+  (run 7 (q)
+    (z/fresh
+     (lambda (n)
+       (z/fresh
+        (lambda (out)
+          (fresh ()
+            (faco n out)
+            (== q `(,n ,out))))))))
+  '((0 1) (1 1) (2 2) (3 6) (4 24) (5 120) (6 720)))
