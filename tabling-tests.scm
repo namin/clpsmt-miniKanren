a17289f8d34f17f3ded0157919503c117d30076f
William Byrd <webyrd@gmail.com>
2018-04-22 09:01:49 -0500

    added busted fibo tabling test

--- tabling-tests.scm
+++ tabling-tests.scm
@@ -66,3 +66,46 @@
     (run* (q)
       (facto q 720)))
   '(6))
+
+(test "fibo-1-non-tabled"
+  (letrec ((fibo (lambda (n out)
+                   (conde
+                     ((z/assert `(= ,n 0))
+                      (z/assert `(= ,out 0)))
+                     ((z/assert `(= ,n 1))
+                      (z/assert `(= ,out 1)))
+                     ((z/assert `(> ,n 1))
+                      (fresh (n-1 n-2 r1 r2)
+                        (z/assert `(= (- ,n 1) ,n-1))
+                        (z/assert `(= (- ,n 2) ,n-2))
+                        (z/assert `(= (+ ,r1 ,r2) ,out))
+                        (fibo n-1 r1)
+                        (fibo n-2 r2)))))))
+    (run 7 (q)
+      (fresh (n out)
+        (fibo n out)
+        (== q `(,n ,out)))))
+  '((0 0) (1 1) (2 1) (3 2) (4 3) (5 5) (6 8)))
+
+;;; Busted!
+;;
+;; Seems to diverge on the first recursive case
+(test "fibo-1-tabled"
+  (letrec ((fibo (tabled (n out)
+                   (conde
+                     ((z/assert `(= ,n 0))
+                      (z/assert `(= ,out 0)))
+                     ((z/assert `(= ,n 1))
+                      (z/assert `(= ,out 1)))
+                     ((z/assert `(> ,n 1))
+                      (fresh (n-1 n-2 r1 r2)
+                        (z/assert `(= (- ,n 1) ,n-1))
+                        (z/assert `(= (- ,n 2) ,n-2))
+                        (z/assert `(= (+ ,r1 ,r2) ,out))
+                        (fibo n-1 r1)
+                        (fibo n-2 r2)))))))
+    (run 3 (q)
+      (fresh (n out)
+        (fibo n out)
+        (== q `(,n ,out)))))
+  '((0 0) (1 1) (2 1)))
