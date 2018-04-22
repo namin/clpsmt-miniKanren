e60726bb2141921cee643e77b6a370b1f213bc10
Nada Amin <namin@alum.mit.edu>
2018-04-22 15:13:18 +0100

    works by reordering @webyrd

--- tabling-tests.scm
+++ tabling-tests.scm
@@ -87,10 +87,7 @@
         (== q `(,n ,out)))))
   '((0 0) (1 1) (2 1) (3 2) (4 3) (5 5) (6 8)))
 
-;;; Busted!
-;;
-;; Seems to diverge on the first recursive case
-(todo "fibo-1-tabled"
+(test "fibo-1-tabled"
   (letrec ((fibo (tabled (n out)
                    (conde
                      ((z/assert `(= ,n 0))
@@ -101,11 +98,11 @@
                       (fresh (n-1 n-2 r1 r2)
                         (z/assert `(= (- ,n 1) ,n-1))
                         (z/assert `(= (- ,n 2) ,n-2))
-                        (z/assert `(= (+ ,r1 ,r2) ,out))
                         (fibo n-1 r1)
-                        (fibo n-2 r2)))))))
-    (run 3 (q)
+                        (fibo n-2 r2)
+                        (z/assert `(= (+ ,r1 ,r2) ,out))))))))
+    (run 7 (q)
       (fresh (n out)
         (fibo n out)
         (== q `(,n ,out)))))
-  '((0 0) (1 1) (2 1)))
+  '((0 0) (1 1) (2 1) (3 2) (4 3) (5 5) (6 8)))
