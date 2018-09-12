cac6616d010dc0574d0279dea34908651fe618dd
William Byrd <webyrd@gmail.com>
2018-09-11 19:55:10 -0500

    fixing errors and adding comments

--- radi.scm
+++ radi.scm
@@ -7,29 +7,29 @@
 
 (define (mapo r xs f)
   (conde
-   [(== xs '()) (== r '())]
-   [(fresh [y ys z zs]
-      (conso y ys xs)
-      (conso z zs r)
-      (f y z)
-      (mapo zs ys f))]))
+    [(== xs '()) (== r '())]
+    [(fresh [y ys z zs]
+       (conso y ys xs)
+       (conso z zs r)
+       (f y z)
+       (mapo zs ys f))]))
 
 (define (foldlo f x xs r)
   (conde
-   [(== xs '()) (== x r)]
-   [(fresh [y ys z]
-      (conso y ys xs)
-      (f x y z)
-      (foldlo f z ys r))]))
+    [(== xs '()) (== x r)]
+    [(fresh [y ys z]
+       (conso y ys xs)
+       (f x y z)
+       (foldlo f z ys r))]))
 
 (define (keyso s ks)
   (conde
-   [(== s '()) (== ks '())]
-   [(fresh [kv r k v kr]
-      (conso kv r s)
-      (== kv `(,k ,v))
-      (conso k kr ks)
-      (keyso r kr))]))
+    [(== s '()) (== ks '())]
+    [(fresh [kv r k v kr]
+       (conso kv r s)
+       (== kv `(,k ,v))
+       (conso k kr ks)
+       (keyso r kr))]))
 
 (define (maxo x y z)
   (conde
@@ -44,37 +44,37 @@
 
 (define (lookupo x bs out)
   (conde
-   [(== bs '()) (== out #f)]
-   [(fresh [b br y v]
-      (conso b br bs)
-      (== b `(,y ,v))
-      (conde
-       [(== y x) (== v out)]
-       [(=/= y x) (lookupo x br out)]))]))
+    [(== bs '()) (== out #f)]
+    [(fresh [b br y v]
+       (conso b br bs)
+       (== b `(,y ,v))
+       (conde
+         [(== y x) (== v out)]
+         [(=/= y x) (lookupo x br out)]))]))
 
 (define (ino x xs)
   (fresh [y ys]
     (conso y ys xs)
     (conde
-     [(== y x)]
-     [(=/= y x)
-      (ino x ys)])))
+      [(== y x)]
+      [(=/= y x)
+       (ino x ys)])))
 
 (define (not-ino x xs)
   (conde
-   [(== xs '())]
-   [(fresh [y ys]
-      (conso y ys xs)
-      (=/= y x)
-      (not-ino x ys))]))
+    [(== xs '())]
+    [(fresh [y ys]
+       (conso y ys xs)
+       (=/= y x)
+       (not-ino x ys))]))
 
 (define (includo a b)
   (conde
-   [(== a '())]
-   [(fresh [a1 ar]
-      (conso a1 ar a)
-      (ino a1 b)
-      (includo ar b))]))
+    [(== a '())]
+    [(fresh [a1 ar]
+       (conso a1 ar a)
+       (ino a1 b)
+       (includo ar b))]))
 
 (define (set-equivo a b)
   (fresh ()
@@ -85,26 +85,26 @@
   (fresh [a1 ar]
     (conso a1 ar a)
     (conde
-     [(not-ino a1 b)]
-     [(ino a1 b) (excludo ar b)])))
+      [(not-ino a1 b)]
+      [(ino a1 b) (excludo ar b)])))
 
 (define (not-set-equivo a b)
   (conde
-   [(excludo a b)]
-   [(includo a b) (excludo b a)]))
+    [(excludo a b)]
+    [(includo a b) (excludo b a)]))
 
 (define (set-uniono a b c)
   (conde
-   [(== a '()) (== b c)]
-   [(fresh [xa ra]
-      (conso xa ra a)
-      (conde
-       [(ino xa b)
-        (set-uniono ra b c)]
-       [(not-ino xa b)
-        (fresh [rc]
-          (conso xa rc c)
-          (set-uniono ra b rc))]))]))
+    [(== a '()) (== b c)]
+    [(fresh [xa ra]
+       (conso xa ra a)
+       (conde
+         [(ino xa b)
+          (set-uniono ra b c)]
+         [(not-ino xa b)
+          (fresh [rc]
+            (conso xa rc c)
+            (set-uniono ra b rc))]))]))
 
 (define (set-unionso xs out)
   (foldlo set-uniono
@@ -127,18 +127,18 @@
 
 (define (combo f u os1 s1 s2 s3)
   (conde
-   [(== s2 '())
-    (== s3 '())]
-   [(== s1 '())
-    (fresh [a2 r2]
-      (conso a2 r2 s2)
-      (combo f u os1 os1 r2 s3))]
-   [(fresh [a1 r1 a2 r2 sa sm]
-      (conso a1 r1 s1)
-      (conso a2 r2 s2)
-      (f a1 a2 sa)
-      (combo f u os1 r1 s2 sm)
-      (u sa sm s3))]))
+    [(== s2 '())
+     (== s3 '())]
+    [(== s1 '())
+     (fresh [a2 r2]
+       (conso a2 r2 s2)
+       (combo f u os1 os1 r2 s3))]
+    [(fresh [a1 r1 a2 r2 sa sm]
+       (conso a1 r1 s1)
+       (conso a2 r2 s2)
+       (f a1 a2 sa)
+       (combo f u os1 r1 s2 sm)
+       (u sa sm s3))]))
 
 (define (combino f s1 s2 s3)
   (combo f set-uniono s1 s1 s2 s3))
@@ -158,15 +158,18 @@
 (define (plusso s1 s2 s3)
   (combino plusdo s1 s2 s3))
 
+;;; WEB this is messed up!  First two clauses overlap!
+;;; Why aren't there 8 cases?
 (define (timeso a b c)
   (conde
-   [(== a 'zer) (== c 'zer)]
-   [(== b 'zer) (== c 'zer)]
-   [(== a 'neg) (== b 'neg) (== c 'pos)]
-   [(== a 'neg) (== b 'pos) (== c 'neg)]
-   [(== a 'pos) (== b 'neg) (== c 'neg)]
-   [(== a 'pos) (== b 'pos) (== c 'pos)]))
+    [(== a 'zer) (== c 'zer)]
+    [(== b 'zer) (== c 'zer)]
+    [(== a 'neg) (== b 'neg) (== c 'pos)]
+    [(== a 'neg) (== b 'pos) (== c 'neg)]
+    [(== a 'pos) (== b 'neg) (== c 'neg)]
+    [(== a 'pos) (== b 'pos) (== c 'pos)]))
 
+;;; WEB broken!  What is [c] supposed to be?? `(,c)?  Something else?
 (define (timesdo a b s)
   (fresh [c]
     (timeso a b c)
@@ -215,72 +218,72 @@
 
 (define (adivalo e s ocache icache out)
   (conde
-   [(fresh [x l]
-      (== `(var ,x) e)
-      (lookupo x s l)
-      (conde
-       [(== l #f) (== out '())]
-       [(=/= l #f) (== out `((,l ,s ,icache)))]))]
-   [(fresh [i a]
-      (== `(int ,i) e)
-      (injo i a)
-      (== out `(((aval (,a) ()) ,s ,ocache))))]
-   [(fresh [e1 e2 s1 s2]
-      (== `(plus ,e1 ,e2) e)
-      (adivalo-op2 plusso e1 e2 s ocache icache out))]
-   [(fresh [e1 e2 s1 s2]
-      (== `(times ,e1 ,e2) e)
-      (adivalo-op2 timesso e1 e2 s ocache icache out))]
-   [(fresh [x y e0]
-      (== `(lam ,x ,y ,e0) e)
-      (== out `(((aval () ((,x ,y ,e0))) ,s ,icache))))]
-   [(fresh [e1 e2]
-      (== `(app ,e1 ,e2) e)
-      (fresh [r1 s1]
-        (adivalpto e1 s ocache icache s1)
-        (mapo r1 s1
-          (lambda (a1 o)
-            (fresh [r2 is1 cs1 sp icachep s2]
-              (== a1 `((aval ,is1 ,cs1) ,sp ,icachep))
-              (adivalpto e2 sp ocache icachep s2)
-              (mapo r2 s2
-                    (lambda (a2 o2)
-                      (fresh [r3 v2 sp2 icachep2 is3]
-                        (== a2 `(,v2 ,sp2 ,icachep2))
-                        (mapo r3 cs1
-                              (lambda (a3 o3)
-                                (fresh [x y e0 s0]
-                                  (== a3 `(,x ,y ,e0))
-                                  (== s0 `((,x (aval () ((,x ,y ,e))))
-                                           (,y ,v2) . ,sp2))
-                                  (adivalpto e0 s0 ocache icachep2 o3))))
-                        (set-unionso r3 o2))))
-              (set-unionso r2 o))))
-        (set-unionso r1 out)))]
-   [(fresh [e1 e2 e3]
-      (== `(if0 ,e1 ,e2 ,e3) e)
-      (fresh [r1 icachep s1 s2 s3 si]
-        (adivalpo e1 s ocache icache `(,r1 ,icachep))
-        (adivalo-condo 'zer e2 r1 icachep ocache s1)
-        (adivalo-condo 'pos e3 r1 icachep ocache s2)
-        (adivalo-condo 'neg e3 r1 icachep ocache s3)
-        (set-uniono s1 s2 si)
-        (set-uniono si s3 out)))]))
+    [(fresh [x l]
+       (== `(var ,x) e)
+       (lookupo x s l)
+       (conde
+         [(== l #f) (== out '())]
+         [(=/= l #f) (== out `((,l ,s ,icache)))]))]
+    [(fresh [i a]
+       (== `(int ,i) e)
+       (injo i a)
+       (== out `(((aval (,a) ()) ,s ,ocache))))]
+    [(fresh [e1 e2 s1 s2]
+       (== `(plus ,e1 ,e2) e)
+       (adivalo-op2 plusso e1 e2 s ocache icache out))]
+    [(fresh [e1 e2 s1 s2]
+       (== `(times ,e1 ,e2) e)
+       (adivalo-op2 timesso e1 e2 s ocache icache out))]
+    [(fresh [x y e0]
+       (== `(lam ,x ,y ,e0) e)
+       (== out `(((aval () ((,x ,y ,e0))) ,s ,icache))))]
+    [(fresh [e1 e2]
+       (== `(app ,e1 ,e2) e)
+       (fresh [r1 s1]
+         (adivalpto e1 s ocache icache s1)
+         (mapo r1 s1
+               (lambda (a1 o)
+                 (fresh [r2 is1 cs1 sp icachep s2]
+                   (== a1 `((aval ,is1 ,cs1) ,sp ,icachep))
+                   (adivalpto e2 sp ocache icachep s2)
+                   (mapo r2 s2
+                         (lambda (a2 o2)
+                           (fresh [r3 v2 sp2 icachep2 is3]
+                             (== a2 `(,v2 ,sp2 ,icachep2))
+                             (mapo r3 cs1
+                                   (lambda (a3 o3)
+                                     (fresh [x y e0 s0]
+                                       (== a3 `(,x ,y ,e0))
+                                       (== s0 `((,x (aval () ((,x ,y ,e))))
+                                                (,y ,v2) . ,sp2))
+                                       (adivalpto e0 s0 ocache icachep2 o3))))
+                             (set-unionso r3 o2))))
+                   (set-unionso r2 o))))
+         (set-unionso r1 out)))]
+    [(fresh [e1 e2 e3]
+       (== `(if0 ,e1 ,e2 ,e3) e)
+       (fresh [r1 icachep s1 s2 s3 si]
+         (adivalpo e1 s ocache icache `(,r1 ,icachep))
+         (adivalo-condo 'zer e2 r1 icachep ocache s1)
+         (adivalo-condo 'pos e3 r1 icachep ocache s2)
+         (adivalo-condo 'neg e3 r1 icachep ocache s3)
+         (set-uniono s1 s2 si)
+         (set-uniono si s3 out)))]))
 
  (define (adivalwo e s ocache icache out)
-  (fresh [res r c r-out c-out]
-    (adivalo e s ocache icache res)
-    (mapo r res (lambda (a o)
-                  (fresh [x y z]
-                    (== a `(,x ,y ,z))
-                    (== o `(,x ,y)))))
-    (mapo c res (lambda (a o)
-                  (fresh [x y z]
-                    (== a `(,x ,y ,z))
-                    (== o z))))
-    (set-unionso2 r r-out)
-    (set-unionso c c-out)
-    (== out `(,r-out ,c-out))))
+   (fresh [res r c r-out c-out]
+     (adivalo e s ocache icache res)
+     (mapo r res (lambda (a o)
+                   (fresh [x y z]
+                     (== a `(,x ,y ,z))
+                     (== o `(,x ,y)))))
+     (mapo c res (lambda (a o)
+                   (fresh [x y z]
+                     (== a `(,x ,y ,z))
+                     (== o z))))
+     (set-unionso2 r r-out)
+     (set-unionso c c-out)
+     (== out `(,r-out ,c-out))))
 
 (define (adivalpo e s ocache icache out)
   (fresh [r]
@@ -310,8 +313,8 @@
   (fresh [r cachep]
     (adivalpo e '() cache '() `(,r ,cachep))
     (conde
-     [(set-equivo cache cachep) (== out cache)]
-     [(not-set-equivo cache cachep) (lfppo e cachep out)])))
+      [(set-equivo cache cachep) (== out cache)]
+      [(not-set-equivo cache cachep) (lfppo e cachep out)])))
 
 (define (lfpo e out)
   (lfppo e '() out))
@@ -323,11 +326,11 @@
 
 (define (iterpo n e cache)
   (conde
-   [(== n 0) cache]
-   [(fresh [r cachep n-1]
-      (z/assert `(= (+ 1 ,n-1) ,n))
-      (adivalpo e '() cache '() `(,r ,cachep))
-      (iterpo n-1 e cachep))]))
+    [(== n 0) cache]
+    [(fresh [r cachep n-1]
+       (z/assert `(= (+ 1 ,n-1) ,n))
+       (adivalpo e '() cache '() `(,r ,cachep))
+       (iterpo n-1 e cachep))]))
 
 (define (itero n e out)
   (iterpo n e '() out))
