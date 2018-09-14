65b871db9d0f692ed043314b7cf3a5311fd03bae
William Byrd <webyrd@gmail.com>
2018-09-14 17:38:06 -0500

    added tests

--- radif-tests.scm
+++ radif-tests.scm
@@ -36,3 +36,46 @@
                                                 (plus (var n) (int -1)))))))))
          (n (aval (neg zer pos) ()))))))))
 
+(time
+  (test "radi-efact-backwards-1"
+    (run* [q]
+      (analyzeo efact
+                '(((aval (neg zer pos) ())
+                   ((self (aval () ((self n
+                                          (if0 (var n)
+                                               (int 1)
+                                               (times (var n)
+                                                      (app (var self)
+                                                           (plus (var n) (int -1)))))))))
+                    (n (aval (neg zer pos) ()))))
+                  ((aval (pos) ())
+                   ((self (aval () ((self n
+                                          (if0 (var n)
+                                               (int 1)
+                                               (times (var n)
+                                                      (app (var self)
+                                                           (plus (var n) (int -1)))))))))
+                    (n (aval (neg zer pos) ())))))))
+    '(_.0)))
+
+(time
+  (test "radi-efact-backwards-2"
+    (run* [q]
+      (analyzeo `(app ,fact (int ,q))
+                '(((aval (neg zer pos) ())
+                   ((self (aval () ((self n
+                                          (if0 (var n)
+                                               (int 1)
+                                               (times (var n)
+                                                      (app (var self)
+                                                           (plus (var n) (int -1)))))))))
+                    (n (aval (neg zer pos) ()))))
+                  ((aval (pos) ())
+                   ((self (aval () ((self n
+                                          (if0 (var n)
+                                               (int 1)
+                                               (times (var n)
+                                                      (app (var self)
+                                                           (plus (var n) (int -1)))))))))
+                    (n (aval (neg zer pos) ())))))))
+    '(1)))
