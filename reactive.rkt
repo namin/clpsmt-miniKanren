c416433738fe115f04fb6abd766788555cde1a7b
William Byrd <webyrd@gmail.com>
2018-11-06 00:58:39 -0500

    playing with reactive programming

--- reactive.rkt
+++ reactive.rkt
@@ -3,7 +3,8 @@
 (require "mk.rkt")
 (require graphics/graphics)
 
-(provide draw-line-mult-by-time)
+(provide (all-from-out "mk.rkt")
+         (all-defined-out))
 
 (define pi 3.14159)
 (define two-pi (* pi 2))
@@ -18,6 +19,44 @@
 
 (define (current-milliseconds)
   (inexact->exact (floor (current-inexact-milliseconds))))
+
+(define (draw-bouncing-ball-by-time)
+  (open-graphics)
+  (let ((w (open-viewport "draw-bouncing-ball-by-time" horiz vert)))
+    (dynamic-wind
+        void
+        (let ((ball-size 5.0)              
+              (start-time (modulo (quotient (current-milliseconds) 50) 50)))
+          (let loop ((old-time start-time)
+                     (old-pos (make-posn 0 0))
+                     (x-velocity 2)
+                     (y-velocity 3))
+            (let ((new-time (modulo (quotient (current-milliseconds) 50) 50)))
+              (if (= new-time old-time)
+                  (loop old-time old-pos x-velocity y-velocity)
+                  (let ((ans (list new-time new-time)))
+                    (let ((factor-ls ans))
+                      ((clear-solid-ellipse w)
+                       old-pos
+                       ball-size
+                       ball-size)
+                      
+                      (let ((possible-new-x (+ (posn-x old-pos) x-velocity)))
+                        (let ((x-velocity (if (<= possible-new-x 50)
+                                              x-velocity
+                                              (- x-velocity))))
+                          (let ((new-x (+ (posn-x old-pos) x-velocity)))
+                            (let ((new-pos (make-posn new-x
+                                                      (+ (posn-y old-pos) y-velocity))))
+                              ((draw-solid-ellipse w)
+                               new-pos
+                               ball-size
+                               ball-size)
+                              
+                              (loop new-time new-pos x-velocity y-velocity)))))))))))
+        (begin
+          (close-viewport w)
+          (close-graphics)))))
 
 ;;; Draw lines, whose x/y coordinates reflect all the factors of n,
 ;;; where n is the current time in seconds, mod 50.
