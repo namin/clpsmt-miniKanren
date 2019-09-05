2f3508e4b0212f47ef4b61f66003a77c3eb722d6
Nada Amin <namin@alum.mit.edu>
2019-09-05 10:31:36 -0400

    simplif, non-rel

--- synthesis.scm
+++ synthesis.scm
@@ -16,20 +16,32 @@
                                                     (= y ,(cdr ex))))
                                             exs))
                                  (= y ,shape)))))
-        (== q `(lambda (x) ,shape))))))
+        z/purge
+        (fresh (ax axb)
+          (conde ((== a 1) (== ax 'x))
+                 ((== a 0) (== ax 0))
+                 ((=/= a 1) (=/= a 0) (== ax `(* ,a x))))
+          (conde ((== ax 0) (== axb b))
+                 ((=/= ax 0) (== b 0) (== axb ax))
+                 ((=/= ax 0) (=/= b 0) (== axb `(+ ,ax ,b))))
+          (== q `(lambda (x) ,axb)))))))
 
 
 (test "syn-inc"
   (run* (q) (synthesize q '((1 . 2) (2 . 3))))
-  '((lambda (x) (+ (* 1 x) 1))))
+  '((lambda (x) (+ x 1))))
 
 (test "syn-double"
   (run* (q) (synthesize q '((1 . 2) (2 . 4))))
-  '((lambda (x) (+ (* 2 x) 0))))
+  '((lambda (x) (* 2 x))))
 
 (test "syn-const"
   (run* (q) (synthesize q '((1 . 2) (2 . 2))))
-  '((lambda (x) (+ (* 0 x) 2))))
+  '((lambda (x) 2)))
+
+(test "syn-lin"
+  (run* (q) (synthesize q '((1 . 3) (2 . 5))))
+  '((lambda (x) (+ (* 2 x) 1))))
 
 (test "syn-no"
   (run* (q) (synthesize q '((2 . 3) (3 . 2) (4 . 3))))
